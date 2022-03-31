454b69a71747e026203fc9a2b151a01590e87334
Zachery Wagner <zacherywagner@gmail.com>
2022-03-30 20:46:52 -0700

    added safe check for device support

--- ARAudioDesigner/Support/AppDelegate.swift
+++ ARAudioDesigner/Support/AppDelegate.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import ARKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        guard ARWorldTrackingConfiguration.isSupported else {
+            fatalError("ARKit is not available on this device.")
+        }
+
         return true
     }
 
@@ -28,7 +32,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
